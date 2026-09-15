8faaf70493315ffcdf52c74422f0369dd1f94c7c
gustavosantiagosbraz.@gmail.com <gevora421@gmail.com>
2026-09-15 11:39:05 -0300

    adicionada a pasta 15.09 - variaveis

---
+++ 15.09/script.js
@@ -0,0 +1,39 @@
+// let miranha = "Peter Parker";
+// let numero = 9999999;
+// let numero2 = "9999999";
+// let idade = 17;
+// const nome = "gustavo"
+// nome = "joao"
+
+
+// idade = 20;
+// idade = 21;
+
+
+
+// console.log("O miranha é o " + miranha + "e ele tem " + idade + " anos");
+// crie 4 variaveis contendo:nome, idade e curso
+// exiba no console em unma unica frase
+
+
+let idade = 17;
+let nome = "gustavo";
+let sobrenome = "santiago ";
+let curso = "javascript";
+// console.log("O nome dele  é " + nome + " e o sobrenome dele  é " + sobrenome + "e ele tem " + idade + " anos " + "e faz curso de " + curso)
+
+
+let nome = "gustavo";//string
+let peso = 85; //int
+let altura = 1.82; //float
+let funcionario = false;// bool - true == verdadeiro ou false == falso
+
+let cidade = null ;
+let estado;
+console.log(typeof nome);
+console.log(typeof peso);
+console.log(typeof altura);
+console.log(typeof funcionario);
+
+console.log("a variavel nome tem " + nome + "e é do tipo " 
+)
