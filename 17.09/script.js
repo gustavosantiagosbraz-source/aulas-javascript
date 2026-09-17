38f5c50d0d18e98e8d7c236ea0ea122ffeed4b8c
gustavosantiagosbraz.@gmail.com <gevora421@gmail.com>
2026-09-17 11:58:10 -0300

    exercicios aula 17 finalizados

---
+++ 17.09/script.js
@@ -0,0 +1,111 @@
+// let nota = 6;
+
+// if (nota < 5) {
+//     console.log("Henrique reprovado");
+    
+// } else if (nota < 7) {
+//     console.log(" Henrique de recuperação");
+    
+// } else{
+//     console.log("Henrique aprovado");
+    
+// }
+
+// let temperatura = 25;
+
+// if (temperatura > 30) {
+//     console.log("Está quente");
+
+// } else if (temperatura >= 20 && temperatura <= 30) {
+//     console.log("Temperatura agradável");
+    
+// } else { 
+
+//     console.log("está frio");
+// }
+
+// let velocidade = 75;
+// if (velocidade <= 60) {
+//     console.log("velocidade permitida");
+    
+// } else if (velocidade >= 60 && velocidade <= 80) {
+//     console.log("velociadede acima do permitido");
+    
+// }else{
+//     console.log("velocidade muito alta");
+    
+// }
+
+// let quantidadedeEstoque = 15;
+
+// if (quantidadedeEstoque == 0) {
+//     console.log("produto esgotado");
+    
+// }if (quantidadedeEstoque >= 1 && quantidadedeEstoque <= 10) {
+//     console.log("Estoque baixo")
+// }else {
+
+//     console.log("Estoque disponivel");
+    
+// } 
+    
+// let usuario = "admin"
+// let senha = 1543;
+
+// if (usuario = "admin" && senha == 1543) {
+//     console.log("login realizado com sucesso");
+    
+    
+// }else {
+//     console.log("usuario ou senha incorreto");
+    
+// }
+
+// let hora = 8;
+// if (hora >= 0 && hora <= 11) {
+//     console.log("Bom Dia!!");
+    
+// }else if (hora >= 12 && hora <= 17) {
+//     console.log("Boa Tarde!!")
+
+// }else if (hora >= 18 && hora <= 23) {
+//     console.log("Boa Noite!!");
+    
+// }
+
+// let nota = 3;
+// if (nota == 5) {
+//     console.log("excelente");
+    
+// } else if (nota == 4) {
+//     console.log("muito bom");
+    
+// } else if (nota == 3) {
+//     console.log("bom");
+    
+// } else if (nota == 2) {
+//     console.log("regular");
+    
+// } else if (nota == 1) {
+//     console.log("ruim");
+    
+// } else {
+//     console.log("Nota Inválida");
+    
+// }
+
+let ingresso = "meia";
+
+if (ingresso == "VIP") {
+    console.log("80");
+    
+} else if (ingresso == "inteira") {
+    console.log("40")
+
+}else if (ingresso == "meia") {
+    console.log("20");
+    
+} else{
+    console.log("Ingresso inválido");
+    
+}
