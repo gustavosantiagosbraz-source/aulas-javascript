d1427f6c12bb9ef979f29fc76bdee801b580fe2e
gustavosantiagosbraz.@gmail.com <gevora421@gmail.com>
2026-09-23 11:54:11 -0300

    aula 23/09 estrutura de repetição

---
+++ 23.09/script.js
@@ -0,0 +1,128 @@
+// --------- ESTRUTURAS DE REPETIÇÃO ----------
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+// console.log("6");
+// console.log("7");
+// console.log("8");
+// console.log("9");
+// console.log("10");
+
+// FOR 
+// for (variavel; condicao; incremento) {
+    
+// }
+
+// for (let index = 0; index < 10; index++) {
+// console.log(index);
+    
+// }
+// // DECREMENTO
+// for (let index = 10; index >= 0; index--) {
+// console.log(index);
+    
+
+
+// // INCREMENTO PERSONALIZADO
+// for (let index = 10; index >= 0; index-=2) {
+// console.log(index);
+    
+// }
+
+// for (let index = 1; index <= 20; index++) {
+// console.log(index);
+    
+// }
+
+// for (let index = 50; index >= 5; index-=5) {
+// console.log(index);
+    
+// }
+
+// for (let index = 10; index >= 0; index--) {
+//     console.log(index);
+    
+    
+// }
+// console.log("contagem finalizada");
+// WHILE
+// let contador = 0;
+// while (contador <= 10) {
+    
+//     console.log(contador);
+    
+//     contador++;
+// }
+
+// let contagem = 10;
+// while (contagem <= 20) {
+//     console.log(contagem);
+    
+//     contagem++;
+
+// }
+
+// let contagem = 2;
+// while (contagem <= 20) {
+    
+//       console.log(contagem)
+      
+//       contagem+=2;
+// }
+
+// let n = 10;
+// while (n < 5) {
+//     console.log("WHILE");
+    
+// }
+
+// do {
+//     console.log("DO...WHILE");
+    
+    
+// } while (n < 5);
+
+// let contador = 0;
+// do {
+//     console.log(contador);
+    
+//     contador++;
+// } while (contador <= 10);
+
+// let contagem = 10;
+// do {
+//     console.log(contagem);
+//     contagem++;
+// } while (contagem <= 100);
+
+// let contagem = 10;
+// do {
+//     console.log(contagem);
+//     contagem--;
+// } while (contagem >= 1);
+
+//---------- Array (LISTA) ------------
+let nome = "Gustavo";
+let nome2 = "Santiago";
+let nome3 = "Ordilei";
+let nome4 = "Levi";
+//               0          1          2         3
+let nomes = ["Gustavo", "Santiago", "Ordilei", "Levi"]
+
+console.log(nomes);// mostra a lista completa na mesma linha
+
+console.log(nomes[1]);// mostra o item da posição mencionada entre colchetes
+
+nome4 = "Ana";
+nome[3] = "Ana";// Altero o valor da posição especificada
+
+console.log(nomes.length);//Mostra o tamanho do Array
+
+
+
+
+
+
+ 
