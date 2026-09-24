1afc7e627fd58973af2208b731fdc36001cb46e0
gustavosantiagosbraz.@gmail.com <gevora421@gmail.com>
2026-09-24 11:58:04 -0300

    Aula 24/09 - Array

---
+++ 24.09/script.js
@@ -0,0 +1,91 @@
+//               0        1       2        3         4 
+// let frutas = ["Banana", "Maçã", "Uva", "Morango", "Pitaya" ]
+//         console.log(frutas);
+//         console.log(frutas[0]);
+//         console.log(frutas[3]);
+
+//         console.log(frutas.length);
+        
+//                       0               1                     2            3                4
+    // let cidades = ["São paulo", "São Caetano do Sul", "Rio de janeiro", "Mauá", "São Bernado do Campo"]
+    
+        
+    //     cidades[1] = "São Caetano do Sul";
+    //     console.log(cidades[1]);
+    //     console.log(cidades.length);
+        
+        // for (let index = 0; index < 5; index++) {
+        //    console.log(cidades[index]);
+           
+        // }
+        // for (let index = 0; index < cidades.length; index++) {
+        //    console.log(cidades[index]);
+           
+        // }
+
+        // let nomes =["Gustavo", "ana livia", "erick","pedro", "gabriel", "pietro"];
+        // for (let index = 0; index < nomes; index++) {
+        //    console.log(nomes[index]);
+           
+            
+        // }
+        // let preços =["19,90","20,50","5,50","500,99","4000"]
+        // for (let index = 0; index < preços.length; index++) {
+        //     console.log(preços[index]);
+            
+        // }
+        // let produtos = ["shampoo","condicionador", "creme","perfume", "desodorante"];
+        // let valores = [60, 50, 30, 150, 30];
+        // for (let index = 0; index < produtos.length; index++) {
+        //     console.log("produto: " + produtos[index] +" - valores: R$"+ valores[index] + " ,00");
+            
+            
+        // }
+            
+//------------ Estrutura de Repetição + Estrutura de Decisão-------------
+// let numero = [2,4,6,8,10,12,14,16,18,20]
+//     for (let index = 0; index < numero.lingth; index++) {// lendo o array
+
+    //     if (numeros[index] >= 10) {// verificando se é maior ou igual a 10
+    //         console.log(index[index]);//mostra o numero do array
+            
+    //     }
+        
+    //    }
+    // let numero = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    // for (let index = 0; index < numero.length; index++) {// lendo o array
+    // let sobra = numero[index] % 2;
+
+    // if (sobra == 0) {
+    //     console.log("O nuemro " + numero[index] +" é par");
+        
+    // } else {
+    //       console.log("O nuemro " + numero[index] +" é impar");
+    // }
+    
+    
+    
+    // } 
+
+    // let nota = [1, 2, 3, 4, 5, 6, 7, 8];
+    // for (let index = 0; index < nota.length; index++) {
+    //     if (nota[index] >= 7) {
+    //         console.log("Aprovado");
+            
+    //     } else {
+    //         console.log("reprovado");
+            
+    //     }
+        
+    // }
+// let temperatura = [15, 20, 25, 30, 35, 40, 45]
+// for (let index = 0; index < temperatura.length; index++) {
+//     if (temperatura >= 30) {
+//         console.log("Quente");
+        
+//     } else {
+//         console.log("Frio");
+        
+//     }
+// }
+
